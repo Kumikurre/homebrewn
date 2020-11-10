1f02f7e4ee847a1d37dc23ca6c650dec351e51d3
Atte Jauhiainen <atte.jauhiainen@gmail.com>
2020-11-10 21:48:11 +0200

    add skeleton for frontend

---
+++ frontend/js/main.js
@@ -0,0 +1,25 @@
+var root = document.body
+
+m.render(root, [
+    m("main", [
+        m("h1", {class: "title"}, "This text is rendered from javascript"),
+        m("button", "A button"),
+    ])
+])
+
+
+var get_devices = function() {
+    m.request({
+        method: "GET",
+        url: "/devices"
+    })
+    .then(function(data) {
+        m.render(root.output, [
+            m("output", [
+                m("h2", {class: "title"}, data)
+            ])
+        ])
+    })
+}
+
+get_devices()

bcb954ee9e36495febf14f940dd21b0cb4d157c9
Glukoosi <aleksi@seppanen.dev>
2020-11-22 18:17:22 +0200

    backend: Add basic get, post and delete for measurement data

---
+++ backend/helpers/helpers.go
@@ -0,0 +1,27 @@
+package helpers
+
+import "strconv"
+
+// TempMeasurementRead is used for reading json from posting temp measurement
+type TempMeasurementRead struct {
+	Value           string `json:"value" bson:"value"`
+	MeasurementUnit string `json:"measurement_unit" bson:"measurement_unit"`
+}
+
+// StringToIntConverter converts string to int64
+func StringToIntConverter(timestamp string) int64 {
+	n, err := strconv.ParseInt(timestamp, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	return n
+}
+
+// StringToFloatConverter converts string to float64
+func StringToFloatConverter(stringToConvert string) float64 {
+	n, err := strconv.ParseFloat(stringToConvert, 64)
+	if err != nil {
+		panic(err)
+	}
+	return n
+}
